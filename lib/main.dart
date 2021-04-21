53b791d40987b239f9809ccaa2db9b34e661ecfb
陶帅 <taoshuai@dajiashequ.com>
2021-04-21 18:48:56 +0800

    控件学习

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -6,116 +6,76 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: new RandomWords(),
+    return new CupertinoApp(
+      title: 'HOME',
+      home: new HomePage(),
     );
   }
 }
-class RandomWords extends StatefulWidget {
+
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return new RandomWordsState();
+    return new HomePageState();
   }
-
 }
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
 
+class HomePageState extends State {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold (
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text('Home'),
+      ),
+      body: _buildImageList(context),
+      bottomNavigationBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.home,
+                size: 24,
+              ),
+              label: '首页'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search, size: 24), label: '搜索'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings, size: 24), label: '设置')
         ],
       ),
-      body: _buildSuggestions(),
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-                (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-              .toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
+  Widget _buildImageList(BuildContext context) {
+    return ListView(
+      children: [
+        _buildRow(context, 'a'),
+        _buildRow(context, 'b'),
+        _buildRow(context, 'c'),
+        _buildRow(context, 'd'),
+        _buildRow(context, 'e'),
+      ],
+    );
+  }
+
+  Widget _buildRow(BuildContext context, data) {
+    final textStyle = TextStyle(fontSize: 22, color: CupertinoColors.label);
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.red, Colors.orange[700]]),
+          //背景渐变
+          borderRadius: BorderRadius.circular(3.0),
+          //3像素圆角
+          boxShadow: [
+            //阴影
+            BoxShadow(
+                color: Colors.black54,
+                offset: Offset(2.0, 2.0),
+                blurRadius: 4.0)
+          ]),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Text(data, style: textStyle),
       ),
     );
   }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
 }
